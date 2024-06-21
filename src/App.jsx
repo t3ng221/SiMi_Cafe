01ae65a1968599150b7b56da2a7efb21f0993b1f
Md. Sazedur Rahman <sazedur15-2169@diu.edu.bd>
2024-06-22 01:03:23 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-
-import Navbar from './components/Navbar'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Navbar from "./components/Navbar";
+import React from "react";
 
 const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 700,
+      easing: "ease-in",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-x-hidden">
-       <Navbar />
+      <Navbar />
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
