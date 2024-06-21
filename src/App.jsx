84046c2a9972229fe6b1d0c9b282d020444f15bb
Md. Sazedur Rahman <sazedur15-2169@diu.edu.bd>
2024-06-22 00:40:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-x-hidden">
+       <Navbar />
+    </div>
+  )
+}
+
+export default App
+
